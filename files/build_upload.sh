9659401758793d6c463caa11c1faecb0a4c9df69
Rupert Broad <rupert.broad@gmail.com>
2019-02-25 08:50:47 +0100

    add get auth token

--- files/build_upload.sh
+++ files/build_upload.sh
@@ -3,7 +3,7 @@ version=$(date "+%Y%m%d%H%M%S")
 
 
 docker-compose build
-# $(aws ecr get-login --no-include-email --region eu-west-1)
+$(aws ecr get-login --no-include-email --region eu-west-1)
 docker tag csapi:latest 771529465472.dkr.ecr.eu-west-1.amazonaws.com/csapi:latest
 docker tag csapi:latest 771529465472.dkr.ecr.eu-west-1.amazonaws.com/csapi:$version
 docker push 771529465472.dkr.ecr.eu-west-1.amazonaws.com/csapi:latest
